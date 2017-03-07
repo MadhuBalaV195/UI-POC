93c7c811368aceb75efbfc9ff0774c9c7be6978b
Madhu Bala <madhu.bala@tavant.com>
2017-03-07 18:44:50 +0530

    cleared errors

--- quickstart-master/src/app/updateprofile/updateprofile.component.ts
+++ quickstart-master/src/app/updateprofile/updateprofile.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import {ANGULAR_TABS_DIRECTIVES, TabInterface} from "angular2-tabs/core";
+
 
 @Component ({
     selector: 'updateprofile-comp',
-    templateUrl: 'app/updateprofile/updateprofile.component.html',
-     directives: [ANGULAR_TABS_DIRECTIVES]
+    templateUrl: 'app/updateprofile/updateprofile.component.html'
+   
 })
 
 export class UpdateProfileComponent {
-    tabOne: TabInterface = {id: "test", title: 'test Title', canActivate: () => {return true;}}
+    
 
 }
