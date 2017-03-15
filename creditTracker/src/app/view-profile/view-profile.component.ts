96cd25ea6359f80695ada9482bd70cc596f9ab4d
Indumathi P <indumathi2294@gmail.com>
2017-03-15 15:59:15 +0530

    adding scores

--- creditTracker/src/app/view-profile/view-profile.component.ts
+++ creditTracker/src/app/view-profile/view-profile.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-profile.component.css']
 })
 export class ViewProfileComponent implements OnInit {
-
+  //  myModel1: number = null;
+  //  myModel2: number = null;
+  //  myModel3: number = null;
+  //  myModel4: number = null;
   constructor() { }
 
   ngOnInit() {
