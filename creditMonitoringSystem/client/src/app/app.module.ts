dfbfaa6c70c12c8692b8879d64fb8ffaf3540858
Indumathi P <indumathi2294@gmail.com>
2017-05-24 15:44:30 +0530

    chaning poc to quality of deliverables

--- creditMonitoringSystem/client/src/app/app.module.ts
+++ creditMonitoringSystem/client/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ControlPanelComponent } from './control-panel/control-panel.component';
 import { TabComponent } from './tab/tab.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { UpdateProfileComponent } from './update-profile/update-profile.component';
-import { PocDetailsComponent } from './poc-details/poc-details.component';
+import { qualityofdeliverablesComponent } from './quality-of-deliverables/quality-of-deliverables.component';
 import { TrainingsAttendedComponent } from './trainings-attended/trainings-attended.component';
 import { TrainingsTakenComponent } from './trainings-taken/trainings-taken.component';
 import { AwardsComponent } from './awards/awards.component';
@@ -30,7 +30,7 @@ import { UserDashboardComponent } from './user-dashboard/user-dashboard.componen
     TabComponent,
     TabsComponent,
     UpdateProfileComponent,
-    PocDetailsComponent,
+    qualityofdeliverablesComponent,
     TrainingsAttendedComponent,
     TrainingsTakenComponent,
     AwardsComponent,
