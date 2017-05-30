b0add6df2ca350bce5c7d1698c4a8a65ff0102bd
Ankita Anand <ankita.anand@tavant.com>
2017-05-30 13:51:14 +0530

    view profile added

--- creditMonitoringSystem/client/src/app/app.module.ts
+++ creditMonitoringSystem/client/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ControlPanelComponent } from './control-panel/control-panel.component';
 import { TabComponent } from './tab/tab.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { UpdateProfileComponent } from './update-profile/update-profile.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
 import { qualityofdeliverablesComponent } from './quality-of-deliverables/quality-of-deliverables.component';
 import { TrainingsAttendedComponent } from './trainings-attended/trainings-attended.component';
 import { TrainingsTakenComponent } from './trainings-taken/trainings-taken.component';
@@ -30,6 +31,7 @@ import { UserDashboardComponent } from './user-dashboard/user-dashboard.componen
     TabComponent,
     TabsComponent,
     UpdateProfileComponent,
+    ViewProfileComponent,
     qualityofdeliverablesComponent,
     TrainingsAttendedComponent,
     TrainingsTakenComponent,
